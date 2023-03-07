43c7cc8184ee329691f5f314c312950140810732
EC2 Default User <tnakahashi2018@gmail.com>
2023-03-07 00:35:50 +0000

    ch05追加

---
+++ ch05/case.rb
@@ -0,0 +1,12 @@
+tags = ["A", "IMG", "PRE"]
+tags.each do |tagname|
+  case tagname
+  when "P", "A", "I", "B", "BLOCKQUOTE"
+    puts "#{tagname} has a child."
+  when "IMG", "BR"
+    puts "#{tagname} has no child."
+  else
+    puts "#{tagname} cannot be used."
+  end
+end
+    
---
+++ ch05/if_else.rb
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+if a > b then
+  puts "aはbより大きい"
+elsif a < b then
+  puts "bはaより大きい"
+else
+  puts "aはbと同じ"
+end
